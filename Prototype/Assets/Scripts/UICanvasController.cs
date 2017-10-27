1224fda5953c5ab5455a5199bcfcc2019e3afceb
ncalderella <ncaldere@uccs.edu>
2017-10-27 10:57:52 -0600

    made survey scene

--- Prototype/Assets/Scripts/UICanvasController.cs
+++ Prototype/Assets/Scripts/UICanvasController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UICanvasController : MonoBehaviour
+    
 {
+    public GameObject[] Canvases = new GameObject[6];
+       
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        Canvases = GameObject.FindGameObjectsWithTag("Canvas");
+        foreach(GameObject canvas in Canvases)
+        {
+            if(canvas.name != "MainMenu")
+            {
+                canvas.SetActive(false);
+            }
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -20,5 +31,20 @@ public class UICanvasController : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadCanvas(string canvasName)
+    {
+        foreach(GameObject canvas in Canvases)
+        {
+            if(canvas.name == canvasName)
+            {
+                canvas.SetActive(true);
+            }
+            else
+            {
+                canvas.SetActive(false);
+            }
+        }
     }
 }
