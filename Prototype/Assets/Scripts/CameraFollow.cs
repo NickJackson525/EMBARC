812470c4782bc0285f6f5c76f31f76686a2555b3
NickJackson525 <njacksonco>
2017-10-29 00:38:41 -0600

    Created transition between some of the planets, added framework for the business model canvas.

---
+++ Prototype/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject spaceship;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spaceship = GameObject.FindGameObjectWithTag("Spaceship");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (spaceship.GetComponent<SpaceshipMovement>().isMoving)
+        {
+            transform.position = new Vector3(spaceship.transform.position.x, spaceship.transform.position.y, -10f);
+        }
+        else
+        {
+            transform.position = new Vector3(spaceship.transform.position.x - 7.5f, transform.position.y, -10f);
+        }
+	}
+}
