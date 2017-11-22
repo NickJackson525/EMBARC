b37723c62989227ab0dff417c466c785e0d50252
LAPTOP-H5PPJI5D\njack <njacksonco@gmail.com>
2017-11-21 22:52:29 -0700

    Added place markers, added dropdown info menus, added other 2 planets to the journey

--- Prototype/Assets/Scripts/GameManager.cs
+++ Prototype/Assets/Scripts/GameManager.cs
@@ -33,19 +33,19 @@ public class GameManager
         {
             stage.THREE, new Dictionary<nodeNumber, Vector3>
             {
-                {nodeNumber.ONE, new Vector3(-42.2f, -19f, 0f)},
-                {nodeNumber.TWO, new Vector3(-40.6f, -9.8f, 0f)},
-                {nodeNumber.THREE, new Vector3(-39.3f, 1.8f, 0f)},
-                {nodeNumber.FOUR, new Vector3(-39.4f, 15f, 0f)}
+                {nodeNumber.ONE, new Vector3(-7.2f, -18.1f, 0f)},
+                {nodeNumber.TWO, new Vector3(16.6f, -4.9f, 0f)},
+                {nodeNumber.THREE, new Vector3(26.4f, 9f, 0f)},
+                {nodeNumber.FOUR, new Vector3(26.7f, 24.1f, 0f)}
             }
         },
         {
             stage.FOUR, new Dictionary<nodeNumber, Vector3>
             {
-                {nodeNumber.ONE, new Vector3(-42.2f, -19f, 0f)},
-                {nodeNumber.TWO, new Vector3(-40.6f, -9.8f, 0f)},
-                {nodeNumber.THREE, new Vector3(-39.3f, 1.8f, 0f)},
-                {nodeNumber.FOUR, new Vector3(-39.4f, 15f, 0f)}
+                {nodeNumber.ONE, new Vector3(26.7f, 24.1f, 0f)},
+                {nodeNumber.TWO, new Vector3(47.1f, 10.4f, 0f)},
+                {nodeNumber.THREE, new Vector3(54.6f,-4.9f, 0f)},
+                {nodeNumber.FOUR, new Vector3(54.7f, -16.6f, 0f)}
             }
         },
     };
---
+++ Prototype/Assets/Scripts/ProgressDropdown.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressDropdown : MonoBehaviour
+{
+    public GameObject panel;
+    public int timer = 0;
+    bool openDropdown = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(timer > 0)
+        {
+            timer--;
+        }
+
+		if(openDropdown)
+        {
+            if(panel.transform.localScale.y < 1)
+            {
+                if(timer % 2 == 0)
+                {
+                    panel.transform.localScale = new Vector3(panel.transform.localScale.x, panel.transform.localScale.y + .1f, panel.transform.localScale.z);
+
+                    if (panel.transform.localScale.y < 0)
+                    {
+                        panel.transform.localScale = new Vector3(panel.transform.localScale.x, 1, panel.transform.localScale.z);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if(panel.transform.localScale.y > 0)
+            {
+                if (timer % 2 == 0)
+                {
+                    panel.transform.localScale = new Vector3(panel.transform.localScale.x, panel.transform.localScale.y - .1f, panel.transform.localScale.z);
+
+                    if(panel.transform.localScale.y < 0)
+                    {
+                        panel.transform.localScale = new Vector3(panel.transform.localScale.x, 0, panel.transform.localScale.z);
+                    }
+                }
+            }
+        }
+	}
+
+    private void OnMouseEnter()
+    {
+        openDropdown = true;
+        timer = 30;
+    }
+
+    private void OnMouseExit()
+    {
+        openDropdown = false;
+        timer = 30;
+    }
+}
